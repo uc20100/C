e1b1bb472340f375eb3ab83fa5af9a2d67b4a168
UC <33086329+uc20100@users.noreply.github.com>
2022-08-25 13:16:08 +0400

    Изменили общий файл README.md и добавили задачи № 41, 43

---
+++ task041/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.  
+// 0, 7, 8, -2, -2 -> 2  
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] myArray = new int[0];
+
+SetArray(ref myArray);
+Console.WriteLine($"[{String.Join(",", myArray)}] -> {CheckNumberMoreZero(myArray)}");
+
+
+
+int CheckNumberMoreZero(int[] array)
+{
+    int count = 0;
+    foreach (int i in array)
+    {
+        if (i > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string ReadFromConsole()
+{
+    string result = string.Empty;
+    while (true)
+    {
+        var k = Console.ReadKey(true);
+        switch (k.Key)
+        {
+            case ConsoleKey.Backspace:
+                if (result.Length > 0)
+                {
+                    result = result.Remove(startIndex: result.Length - 1, count: 1);
+                    Console.Write(value: $"{k.KeyChar} {k.KeyChar}");
+                }
+                break;
+            case ConsoleKey.Enter:
+                Console.WriteLine();
+                return result;
+            case ConsoleKey.Escape:
+                result = "out";
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.WriteLine(value: "Нажали Esc, выходим из цикла ввода массива  ");
+                return result;
+            default:
+                if (char.IsDigit(c: k.KeyChar) || (k.KeyChar == '-' && result.Length == 0))
+                {
+                    Console.Write(value: k.KeyChar);
+                    result += k.KeyChar;
+                }
+                break;
+        }
+    }
+}
+
+void SetArray(ref int[] array)
+{
+    while (true)
+    {
+        Console.WriteLine($"Введите число №{array.Length + 1} (Для окончания цикла ввода нажмите Esc):");
+        try
+        {
+            string st = ReadFromConsole();
+            if (st == "out")
+            {
+                break;
+            }
+            int number = Convert.ToInt32(st);
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = number;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка преобразования числа");
+        }
+    }
+}
+
+
---
+++ task043/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.  
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+
+double xMinValue = -10;
+double xMaxValue = 10;
+double step = 0.001;
+
+try
+{
+    Console.WriteLine("Введите коэффициент b1: ");
+    int b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент k1: ");
+    int k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент b2: ");
+    int b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент k2: ");
+    int k2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(LineIntersection(xMinValue, xMaxValue, step, b1, k1, b2, k2));
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода");
+}
+
+
+
+
+
+
+string LineIntersection(double xMin, double xMax, double step, int b1, int k1, int b2, int k2)
+{
+    double y1, y2;
+    double x = xMin;
+    while (true)
+    {
+        y1 = Math.Round((k1 * x + b1), 3);
+        y2 = Math.Round((k2 * x + b2), 3);
+        if (y1 != y2 && x < xMax)
+        {
+            // Console.WriteLine($"если y1!=y2 х={Math.Round(x, 3)}, y1={Math.Round(y1, 3)}, y2={Math.Round(y2, 3)}");
+            x += step;
+        }
+        else
+        {
+            if (x >= xMax)
+            {
+                return $"В диапазоне переменной Х от {Math.Round(xMin, 2)} до {Math.Round(xMax, 2)}, нет пересечений";
+            }
+            else
+            {
+                return $"Пересечение отрезков = (x: {Math.Round(x, 2)}, y: {Math.Round(y1, 2)})";
+            }
+        }
+    }
+}
+
